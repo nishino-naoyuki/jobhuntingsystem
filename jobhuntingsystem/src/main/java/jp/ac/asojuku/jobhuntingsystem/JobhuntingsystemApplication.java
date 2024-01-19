a881891678afbf8de1b494835f9e54e010c0a9b6
nishino <nishino@asojuku.ac.jp>
2024-01-19 16:31:41 +0900

    initial commit

---
+++ jobhuntingsystem/src/main/java/jp/ac/asojuku/jobhuntingsystem/JobhuntingsystemApplication.java
@@ -0,0 +1,13 @@
+package jp.ac.asojuku.jobhuntingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobhuntingsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobhuntingsystemApplication.class, args);
+	}
+
+}
---
+++ jobhuntingsystem/src/test/java/jp/ac/asojuku/jobhuntingsystem/JobhuntingsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.asojuku.jobhuntingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobhuntingsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
